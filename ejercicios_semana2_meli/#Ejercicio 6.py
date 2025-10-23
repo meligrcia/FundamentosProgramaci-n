d9a975ca0920cbbb0792ddf7d2c9d5c939b3305b
meligrcia <itzmelinagarcia@gmail.com>
2025-10-23 07:26:05 -0600

    Add files via upload

---
+++ ejercicios_semana2_meli/#Ejercicio 6.py
@@ -0,0 +1,31 @@
+#Ejercicio 6. OPERADORES ARITMETICOS
+print("Ejercicio 6. OPERADORES ARITMETICOS")
+# SUMA (+): Vamos a sumar dos numeros
+numero1 = 42
+numero2 = 22
+suma = numero1 + numero2
+print("Operador suma:", suma)
+
+# RESTA (-): ahora vamos a restar
+resta = numero1 - numero2
+print("Operador resta:", resta)
+
+# MULTIPLICACION (*): multiplicamos dos numeros
+multiplicacion = numero1 * numero2
+print("Operador multiplicacion:", multiplicacion)
+
+# DIVISION (/): dividimos y obtenemos resultado con decimales
+division = numero1 / numero2
+print("Operador division:", division)
+
+# DIVISION ENTERA (//): dividimos pero solo queremos la parte entera
+division_entera = 10 // 3
+print("Operador division entera:", division_entera)
+
+# (%): obtiene el residuo (lo que sobra) de una division
+residuo = 10 % 3
+print("Operador residuo:", residuo)
+
+# POTENCIA (**): elevar un numero a una potencia
+potencia = 2 ** 3
+print("Operador potencia", potencia)
---
+++ ejercicios_semana2_meli/Ejercicio 7.py
@@ -0,0 +1,31 @@
+print("Ejercicio 7. Operadores de comparacion")
+
+print("He aprobado o no la materia?")
+# MAYOR O IGUAL (>=): La calificacion minima para pasar es 70
+calificacion = 70
+resultado5 = calificacion >= 70
+print("Aprobe?:", resultado5)
+
+# MAYOR (>): La calificacion minima para pasar es 70
+resultado6 = calificacion > 70
+print("Aprobe?", resultado6)
+
+# Vamos a comparar estos dos numeros
+mi_edad = 17
+edad_minima = 18
+
+# IGUAL A (==): Pregunta: Los numeros son iguales?
+resultado1 = mi_edad == 15
+print("\nSoy mayor de edad? (==):", resultado1)
+
+# DIFERENTE DE (!=): Pregunta: Los numeros son diferentes?
+resultado2 = mi_edad != 20
+print("Tengo 18 anios? (!=:", resultado2)
+
+# MENOR QUE (<): Pregunta: El primer numero es menor?
+resultado3 = mi_edad < 18
+print("Mi edad es menor que 18? (<):", resultado3)
+
+# MENOR O IGUAL (<=): Pregunta: Es menor o igual?
+resultado4 = mi_edad <= 10
+print("Mi edad es menor o igual a 10 (<=):", resultado4)
